c953ffe818d7ee25d735c03cb39ea9c235cde681
lwj657 <334715403@qq.com>
2022-04-24 10:33:58 +0800

    修改：elementui大小

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import '@/styles/index.scss' // global css
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-app.use(ElementPlus, { size: 'small', locale: zhCn })
+app.use(ElementPlus, { size: 'mini', locale: zhCn })
 
 // global mixin
 // import elementMixin from '@/mixins/elementMixin'
